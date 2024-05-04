a69c7dddb6588f8292bad895690019284ac676a5
wildanvin <danvin.jim@gmail.com>
2024-05-03 23:11:05 -0500

    Improve checkbox

--- packages/nextjs/app/myMultiSigs/components/DisplayMultiSigs.tsx
+++ packages/nextjs/app/myMultiSigs/components/DisplayMultiSigs.tsx
@@ -22,13 +22,42 @@ export const DisplayMultiSigs: React.FC = () => {
             <ul className="space-y-2">
               {walletAddresses.map((address, index) => (
                 <li key={index} className="flex items-center space-x-2">
-                  <input
+                  {/* <input
                     type="checkbox"
                     checked={selectedWallet === address}
                     onChange={() => handleSelectionChange(address)}
                     className="w-8 h-8 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-                  />
-                  <Address address={address} format="long" />
+                  /> */}
+                  <div className="inline-flex items-center">
+                    <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+                      <input
+                        type="checkbox"
+                        className="peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-200 transition-all before:absolute before:top-1/2 before:left-1/2 before:block before:h-12 before:w-12 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:bg-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                        id="check"
+                        checked={selectedWallet === address}
+                        onChange={() => handleSelectionChange(address)}
+                      />
+                      <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-3.5 w-3.5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          stroke="currentColor"
+                          strokeWidth="1"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </span>
+                    </label>
+                    <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
+                      <Address address={address} format="short" />
+                    </label>
+                  </div>
                 </li>
               ))}
             </ul>
