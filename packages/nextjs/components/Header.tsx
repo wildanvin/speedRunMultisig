05dcd86ec0cde12ee688aedbd6a8fdf60d67b3d9
wildanvin <danvin.jim@gmail.com>
2024-05-07 18:59:56 -0500

    remove debug page

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -52,11 +52,11 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/pool",
     icon: <Bars3CenterLeftIcon className="h-4 w-4" />,
   },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
+  // {
+  //   label: "Debug Contracts",
+  //   href: "/debug",
+  //   icon: <BugAntIcon className="h-4 w-4" />,
+  // },
 ];
 
 export const HeaderMenuLinks = () => {
